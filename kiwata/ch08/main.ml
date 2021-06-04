18cea16584b2eb20e705e897ce1452a12f79b6b8
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-06-05 03:48:01 +0900

    fix

--- kiwata/ch08/main.ml
+++ kiwata/ch08/main.ml
@@ -9,6 +9,6 @@ type value = {name = "Fujiyoshi"; tennsuu = 90; seiseki = "seiseki"};;
 
 let tuchi gakusei = match gakusei with
     {name = n; tennsuu = t; seiseki = s} ->
-        n ^ "さんの成績は" ^ string_of_int t ^ "点で、成績は" ^ s "です。";;
+        n ^ "さんの成績は" ^ string_of_int t ^ "点で、成績は" ^ s ^ "です。";;
 
 tuchi {name = "Fujiyoshi"; tennsuu = 90; seiseki = "B"};;
