bd30b8b39ecf92c0bc81df05bb4aaf16ce20cd7a
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-06-05 05:26:41 +0900

    once

--- kiwata/ch09/main.ml
+++ kiwata/ch09/main.ml
@@ -1,1 +1,19 @@
 (* 気になったプログラムだけ実行して行く *)
+let test value = match value with
+    [] -> 0
+    | first :: rest -> first;;
+
+let values = 1 :: 2 :: 3 :: [];;
+(* 1 が返ってくる *)
+test values;;
+
+let rec contain_zero values = match values with
+    [] -> false
+    | first :: rest -> if first = 0 then true else contain_zero rest;;
+
+(* テスト *) 
+let test1 = contain_zero [] = false;;
+let test2 = contain_zero [0; 2] = true;;
+let test3 = contain_zero [1; 2] = false;;
+let test4 = contain_zero [1; 2; 3; 0; 5; 6; 7] = true;;
+let test5 = contain_zero [1; 2; 3; 4; 5; 6; 7] = false;;
--- kiwata/ch09/question/main.ml
+++ kiwata/ch09/question/main.ml
@@ -1,13 +1,51 @@
 (* 演習問題は基本的に全て解いていく *)
 
 (* 9.1 *)
-
+let season = "春" :: "夏" :: "秋" :: "冬" :: [];;
 
 (* 9.2 *)
+type person_t = {
+    name: string;
+    shincho: float;
+    taiju: float;
+    tsuki: int;
+    hi: int;
+    ketsuekigata: string;
+};;
 
+let person1 = 
+  {name = "浅井"; 
+   shincho = 1.72; 
+   taiju = 58.5; 
+   tsuki = 9; 
+   hi = 17; 
+   ketsuekigata = "A" 
+};;
+ 
+let person2 = { 
+  name = "宮原"; 
+  shincho = 1.63; 
+  taiju = 55.0; 
+  tsuki = 6; 
+  hi = 30; 
+  ketsuekigata = "B" 
+};;
+ 
+let person3 = { 
+  name = "中村"; 
+  shincho = 1.68; 
+  taiju = 63.0; 
+  tsuki = 6; 
+  hi = 6; 
+  ketsuekigata = "O" 
+};;
+
+let persons = person1 :: person2 :: person3 :: [];;
 
 (* 9.3 *)
+let season = ["春"; "夏"; "秋"; "冬"];;
 
+let person = [person1; person2; person3];;
 
 (* 9.4 *)
 
