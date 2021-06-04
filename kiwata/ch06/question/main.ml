534d821143ec2db6916dd8d738626113b3ff1332
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-06-05 01:34:25 +0900

    finish ch06

--- kiwata/ch06/question/main.ml
+++ kiwata/ch06/question/main.ml
@@ -1,4 +1,13 @@
 (* 演習問題は基本的に全て解いていく *)
 
 (* 6.1 *)
+let square x = x *. x;;
+square 3. ;;
 
+let pi = 3.14;;
+let circle r = 2. *. pi *. r;;
+circle 2.;;
+
+let tv bangumi youbi =
+    bangumi ^ "は" ^ youbi ^ "に放映です。";;
+tv "ポケモン" "木曜日";;
