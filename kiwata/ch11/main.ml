de0d850f0a4d391e10b408ba989626c19a9018d1
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-06-06 05:17:57 +0900

    finish ch11

--- kiwata/ch11/main.ml
+++ kiwata/ch11/main.ml
@@ -1,1 +1,26 @@
 (* 気になったプログラムだけ実行して行く *)
+(* 目的 : 受け取った整数までの階乗を計算して返す *)
+(* fac : int -> int *)
+let rec fac value =
+    if value = 0 then 1
+    else value * (fac (value - 1));;
+
+(* テスト *) 
+let test1 = fac 0 = 1;;
+let test2 = fac 1 = 1;;
+let test3 = fac 2 = 2;;
+let test4 = fac 3 = 6;;
+let test5 = fac 4 = 24;;
+let test6 = fac 10 = 3628800;;
+
+(* 目的　: 受け取った二つの整数をから階乗を計算して返す *)
+(* poewr : int -> int -> int *)
+let rec power m n =
+    if n = 0 then 1
+    else m * (power m (n - 1));;
+
+(* テスト *) 
+let test1 = power 3 0 = 1;;
+let test2 = power 3 1 = 3;;
+let test3 = power 3 2 = 9;;
+let test4 = power 3 3 = 27;;
--- kiwata/ch11/question/main.ml
+++ kiwata/ch11/question/main.ml
@@ -1,7 +1,29 @@
 (* 演習問題は基本的に全て解いていく *)
 
 (* 11.1 *)
+(* 目的 : 0 から受け取った整数の二乗までの和を計算して返す *)
+(* sum_of_square : int -> int *)
+let rec sum_of_square value =
+    if value = 0 then 0
+    else value * value + sum_of_square(value - 1);;
 
+(* テスト *) 
+let test1 = sum_of_square 0 = 0;;
+let test2 = sum_of_square 1 = 1;;
+let test3 = sum_of_square 2 = 5;;
+let test4 = sum_of_square 3 = 14;;
+let test5 = sum_of_square 4 = 30;;
 
 (* 11.2 *)
+(* 目的 : 漸化式で定義された値を返す *)
+(* a : int -> int *)
+let rec a n =
+    if n = 0 then 3
+    else 2 * a(n - 1) - 1;;
 
+(* テスト *) 
+let test1 = a 0 = 3;;
+let test2 = a 1 = 5;;
+let test3 = a 2 = 9;;
+let test4 = a 3 = 17;;
+let test5 = a 4 = 33;;
