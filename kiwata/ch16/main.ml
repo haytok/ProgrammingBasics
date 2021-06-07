96af4473eea0ceecbd6efe83ebd65c3494bc52b8
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-06-08 02:44:43 +0900

    once

--- kiwata/ch16/main.ml
+++ kiwata/ch16/main.ml
@@ -1,1 +1,37 @@
 (* 気になったプログラムだけ実行して行く *)
+type distance_t = {
+    kyori: float;
+    total: float;
+};;
+
+let total_distance lst =
+    let rec hojo lst total0 = match lst with
+            [] -> []
+            | first :: rest ->
+                let tmp = first.total +. total0 in
+                    {kyori = first.kyori; total = tmp} :: hojo rest tmp in
+                        hojo lst 0.0;;
+
+(* テスト *) 
+let test = total_distance [{kyori = 0.3; total = 0.}; 
+			   {kyori = 0.9; total = 0.}; 
+			   {kyori = 1.4; total = 0.}; 
+			   {kyori = 0.8; total = 0.}] 
+	   = [{kyori = 0.3; total = 0.3}; 
+	      {kyori = 0.9; total = 1.2}; 
+	      {kyori = 1.4; total = 2.6}; 
+	      {kyori = 0.8; total = 3.4}];;
+
+(*  *)
+(* 目的 : lst の逆順のリスト result を返す *)
+let reverse lst =
+    let rec rev lst result = match lst with
+        [] -> result
+        | first :: rest ->
+            rev rest (first :: result) in
+                rev lst [];;
+
+(* テスト *) 
+let test1 = reverse [] = [];;
+let test2 = reverse [1] = [1];;
+let test3 = reverse [1; 2; 3; 4; 5] = [5; 4; 3; 2; 1];;
--- kiwata/ch16/question/main.ml
+++ kiwata/ch16/question/main.ml
@@ -1,10 +1,36 @@
 (* 演習問題は基本的に全て解いていく *)
 
 (* 16.1 *)
+let sum_list lst =
+    let rec hojo lst total0 = match lst with
+        [] -> []
+        | first :: rest ->
+            (first + total0) :: hojo rest (first + total0)
+                in hojo lst 0;;
 
+(* テスト *) 
+let test1 = sum_list [] = [];;
+let test2 = sum_list [1; 2; 3] = [1; 3; 6];;
+let test3 = sum_list [3; 2; 1; 4] = [3; 5; 6; 10];;
 
 (* 16.2 *)
+(* 目的 :  *)
+(* fold_right は何を返す関数なのかを考えると、f の引数の意味を定義できる *)
+(* それは、つまり fold_right の目的を考えることと同義である *)
+let rec fold_right f lst init = match lst with
+    [] -> init
+    | first :: rest ->
+        f first (fold_right f rest init);;
 
+let fold_left f init lst = match lst with
+    [] -> init
+    | first :: rest ->
+        fold_left f (f init first) rest;;
+
+(* テスト *) 
+let test1 = fold_left (-) 0 [] = 0;;
+let test2 = fold_left (-) 10 [4; 1; 3] = 2;;
+let test3 = fold_left (fun lst a -> a :: lst) [] [1; 2; 3; 4] = [4; 3; 2; 1];;
 
 (* 16.3 *)
 
