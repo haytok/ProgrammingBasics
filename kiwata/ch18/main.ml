667e8ec873a4e28691e5c308eceaf36544f03cf4
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-06-09 06:39:22 +0900

    finish ch18

--- kiwata/ch18/main.ml
+++ kiwata/ch18/main.ml
@@ -1,1 +1,133 @@
 (* 気になったプログラムだけ実行して行く *)
+(*  *)
+(* 八百屋においてある野菜と値段のリストの例 *) 
+let yaoya_list = [
+    ("トマト", 300);
+    ("たまねぎ", 200); 
+    ("にんじん", 150);
+    ("ほうれん草", 200)
+];;
+
+(* price : string -> (string * int) * list -> int option *)
+let rec price item yaoya_list = match yaoya_list with
+    (* この値を 0 にすると、野菜がタダなのか売り切れなのかの判別がつかない。 *)
+    [] -> None
+    | (yasai, nedan) :: rest ->
+        if yasai = item then Some (nedan)
+        else price item rest;;
+
+(* テスト *) 
+let test1 = price "トマト" yaoya_list = Some (300);;
+let test2 = price "じゃがいも" yaoya_list = None;;
+
+(*  *)
+let yaoya_list = [
+    ("トマト", 300);
+    ("たまねぎ", 200); 
+    ("にんじん", 150);
+    ("ほうれん草", 200)
+];;
+(* 目的 : 野菜のリストと八百屋のリストを受け取ると、野菜のリストに入っている野菜を全部買った時の合計の値段を返す *)
+(* 例外処理もどきの実装 *)
+(* match hogehoge with fugafuga の hogehoge は for hoge in items における hoge のイメージ *)
+let rec total_price yasai_list yaoya_list = match yasai_list with
+    [] -> 0
+    | first :: rest ->
+        match price first yaoya_list with
+            None -> total_price rest yaoya_list
+            | Some (p) -> p + total_price rest yaoya_list;;
+
+(* テスト *) 
+let test1 = total_price ["トマト"; "にんじん"] yaoya_list = 450;;
+let test2 = total_price ["じゃがいも"; "たまねぎ"; "にんじん"] yaoya_list = 350;;
+
+(*  *)
+(* total_price : string list -> (string * int) list -> int option *)
+let rec total_price yasai_list yaoya_list = match yasai_list with
+    [] -> Some (0)
+    | first :: rest ->
+        match price first yaoya_list with
+            None -> None
+            | Some (p) -> match total_price rest yaoya_list with
+                None -> None
+                | Some(q) -> Some (p + q);;
+
+let total_price yasai_list yaoya_list =
+    let rec hojo yasai_list yaoya_list = match yasai_list with
+        [] -> Some (0)
+        | first :: rest ->
+            match price first yaoya_list with
+                None -> None
+                | Some (p) -> match hojo rest yaoya_list with
+                    None -> None
+                    | Some(q) -> Some (p + q) in
+                        match hojo yasai_list yaoya_list with
+                            None -> 0
+                            | Some (p) -> p;;
+
+(* テスト *) 
+let test1 = total_price ["たまねぎ"; "にんじん"] yaoya_list = 350;;
+let test2 = total_price ["たまねぎ"; "じゃがいも"; "にんじん"] yaoya_list = 0;;
+let test3 = total_price ["トマト"; "にんじん"] yaoya_list = 450;;
+
+(* 上記の例外処理を行って実装したプログラムを改良する *)
+(* ほしい野菜がいつでも八百屋にあると仮定する *)
+let rec price item yaoya_list = match yaoya_list with
+    (* この値を 0 にすると、野菜がタダなのか売り切れなのかの判別がつかない。 *)
+    [] -> 0
+    | (yasai, nedan) :: rest ->
+        if yasai = item then nedan
+        else price item rest;;
+
+let rec total_price yasai_list yaoya_list = match yasai_list with
+    [] -> 0
+    | first :: rest ->
+        price first yaoya_list + total_price rest yaoya_list;;
+
+(* raise を使用してエラーハンドリングを行う *)
+exception Urikire;;
+
+let rec price item yaoya_list = match yaoya_list with
+    (* この値を 0 にすると、野菜がタダなのか売り切れなのかの判別がつかない。 *)
+    [] -> raise Urikire
+    | (yasai, nedan) :: rest ->
+        if yasai = item then nedan
+        else price item rest;;
+
+let total_price yasai_list yaoya_list = 
+    let rec hojo yasai_list = match yasai_list with
+        [] -> 0
+        | first :: rest ->
+            price first yaoya_list + hojo rest in
+                try
+                    hojo yasai_list
+                with Urikire -> 0;;
+
+(* 八百屋においてある野菜と値段のリストの例 *) 
+let yaoya_list = [("トマト", 300); ("たまねぎ", 200); 
+                  ("にんじん", 150); ("ほうれん草", 200)];;
+ 
+(* テスト *) 
+let test1 = total_price ["たまねぎ"; "にんじん"] yaoya_list = 350;;
+let test2 = total_price ["たまねぎ"; "じゃがいも"; "にんじん"] yaoya_list = 0;;
+let test3 = total_price ["トマト"; "にんじん"] yaoya_list = 450;;
+
+(*  *)
+exception Zero;;
+
+let times lst =
+    let rec hojo lst = match lst with
+        [] -> 1
+        | first :: rest ->
+            if first = 0 then raise Zero
+            else first * hojo rest in
+                try
+                    hojo lst
+                with
+                    Zero -> 0;;
+
+(* テスト *) 
+let test1 = times [3; 1; 4] = 12;;
+let test2 = times [0] = 0;;
+let test3 = times [3; 1; 0; 4] = 0;;
+let test4 = times [3; 2; 1; 0] = 0;;
--- kiwata/ch18/question/main.ml
+++ kiwata/ch18/question/main.ml
@@ -1,22 +1,106 @@
 (* 演習問題は基本的に全て解いていく *)
 
 (* 18.1 *)
+(* 人に関する情報を格納するレコード *) 
+type person_t = { 
+  name : string;	(* 名前 *) 
+  shincho : float;	(* 身長 *) 
+  taiju : float;	(* 体重 *) 
+  tsuki : int;		(* 誕生月 *) 
+  hi : int;		(* 誕生日 *) 
+  ketsueki : string;	(* 血液型 *) 
+};;
+ 
+let person1 = 
+  {name = "浅井"; 
+   shincho = 1.72; 
+   taiju = 58.5; 
+   tsuki = 9; 
+   hi = 17; 
+   ketsueki = "A" 
+};;
+ 
+let person2 = { 
+  name = "宮原"; 
+  shincho = 1.63; 
+  taiju = 55.0; 
+  tsuki = 6; 
+  hi = 30; 
+  ketsueki = "B" 
+};;
+ 
+let person3 = { 
+  name = "中村"; 
+  shincho = 1.68; 
+  taiju = 63.0; 
+  tsuki = 6; 
+  hi = 6; 
+  ketsueki = "O" 
+};;
 
+(* person_t list 型のデータの例 *) 
+let lst1 = [person1; person2; person3];;
+let lst2 = [person3; person2; person1];;
+
+(* 目的 : 要素が person_t 型のリストを受け取ると、最初に血液型が A 型の人の要素を返す *)
+(* first_A : person_t list -> person_t option *)
+let rec first_A lst = match lst with
+    [] -> None
+    | first :: rest ->
+        if first.ketsueki = "A" then Some (first)
+        else first_A rest;;
+
+(* テスト *) 
+let test1 = first_A [] = None;;
+let test2 = first_A lst1 = Some (person1);;
+let test3 = first_A lst2 = Some (person1);;
 
 (* 18.2 *)
+let yaoya_list = [
+    ("トマト", 300);
+    ("たまねぎ", 200); 
+    ("にんじん", 150);
+    ("ほうれん草", 200)
+];;
 
+let rec price item yaoya_list = match yaoya_list with
+    (* この値を 0 にすると、野菜がタダなのか売り切れなのかの判別がつかない。 *)
+    [] -> None
+    | (yasai, nedan) :: rest ->
+        if yasai = item then Some (nedan)
+        else price item rest;;
+
+let rec count_urikire_yasai yasai_list yaoya_list = match yasai_list with
+    [] -> 0
+    | first :: rest ->
+        match price first yaoya_list with
+            None -> 1 + count_urikire_yasai rest yaoya_list
+            | Some (p) -> count_urikire_yasai rest yaoya_list;;
+
+(* テスト *) 
+let test1 = count_urikire_yasai ["たまねぎ"; "にんじん"] yaoya_list = 0;;
+let test2 = count_urikire_yasai ["たまねぎ"; "じゃがいも"; "にんじん"] yaoya_list = 1;;
+let test3 = count_urikire_yasai ["しいたけ"; "なす"; "にんじん"] yaoya_list = 2;;
 
 (* 18.3 *)
+let rec assoc ekimei0 lst = match lst with
+    [] -> raise Not_found
+    | (ekimei, kyori) :: rest ->
+        if ekimei = ekimei0 then kyori else assoc ekimei0 rest;;
 
+(* テスト *)
+let test1 = assoc "後楽園" [];;
+let test2 = assoc "後楽園" [("新大塚", 1.2); ("後楽園", 1.8)] = 1.8;;
+let test3 = assoc "池袋" [("新大塚", 1.2); ("後楽園", 1.8)];;
 
 (* 18.4 *)
-
+(* 駅に関する問題なのでパス *)
 
 (* 18.5 *)
-
+(* 駅に関する問題なのでパス *)
 
 (* 18.6 *)
-
+(* 駅に関する問題なのでパス *)
 
 (* 18.7 *)
-
+(* 駅に関する問題なのでパス *)
