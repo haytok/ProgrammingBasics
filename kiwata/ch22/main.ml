3063a877723ab840f7cc3e0b71255e992e87ce4d
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-06-09 20:20:49 +0900

    finish ch22

--- kiwata/ch22/main.ml
+++ kiwata/ch22/main.ml
@@ -1,1 +1,17 @@
 (* 気になったプログラムだけ実行して行く *)
+(* 参照型のデータを定義する *)
+let count = ref 0;;
+let rec fib n =
+    (
+        count := !count + 1;
+        if n < 2 then n else fib (n - 1) + fib (n - 2)
+    );;
+
+let test_1 = fib 8 = 21;;
+
+let a = [|0; 1; 2; 3; 4; 5|];;
+Array.get a 0;;
+a.(0);;
+
+Array.set a 0 1;;
+a.(0) <- 2;;
--- kiwata/ch22/question/main.ml
+++ kiwata/ch22/question/main.ml
@@ -1,7 +1,28 @@
 (* 演習問題は基本的に全て解いていく *)
 
 (* 22.1 *)
+let count = ref (-1);;
+let gensym s =
+    (
+        count := !count + 1;
+        s ^ (string_of_int !count)
+    );;
 
+(* テスト *) 
+let test1 = gensym "a" = "a0";;
+let test2 = gensym "a" = "a1";;
+let test3 = gensym "x" = "x2";;
 
 (* 22.2 *)
+let fib_array lst =
+    let len = Array.length lst in
+        if len > 0 then lst.(0) <- 0;
+        if len > 1 then lst.(1) <- 1;
+        for i = 2 to len - 1 do
+            lst.(i) <- lst.(i - 1) + lst.(i - 2);
+        done;
+        lst;;
 
+(* テスト *) 
+let test = fib_array [|0; 0; 0; 0; 0; 0; 0; 0; 0; 0|] 
+	   = [|0; 1; 1; 2; 3; 5; 8; 13; 21; 34|];;
